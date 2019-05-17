bb0b12ec1937fe09bf660afe46a5231226aff1b0
Nedriia <arthur.favre@hotmail.com>
2019-05-17 10:06:46 +0200

    Clean

--- Assets/Avoidin_B.cs
+++ Assets/Avoidin_B.cs
@@ -6,67 +6,48 @@ namespace FGAE
 {
     public class Avoidin_B : FGAE_CharacterStateBase
     {
-
         public float time_recalc_escapePoint_tmp;
 
         //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             GetCharacterControl(animator);
-
             New_Point_Calc(animator);
-
-
         }
 
         //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            
+        {        
             if (time_recalc_escapePoint_tmp < characterControl.time_recalc_escapePoint)
-            {
                 time_recalc_escapePoint_tmp += Time.deltaTime;
-            }
 
             if (Vector3.Distance(characterControl.avoinding_pos, GetSpaceShip().transform.position) < 1f)
             {
+                //if the spaceShip is within a range and he's not moving anymore, then it means the spaceShip is block. So he calculate a new point
                 if (GetSpaceShip().Velocity.magnitude < 0.2f)
                 {
                     var angle = Mathf.Atan2(characterControl.avoinding_pos.y - GetSpaceShip().transform.position.y, characterControl.avoinding_pos.x - GetSpaceShip().transform.position.x);
                     SetOrient(angle * Mathf.Rad2Deg);
                 }
                 else
-                {
                     animator.SetBool("avoiding", false);
-                }
             }
             else
             {
                 var angle = Mathf.Atan2(characterControl.avoinding_pos.y - GetSpaceShip().transform.position.y, characterControl.avoinding_pos.x - GetSpaceShip().transform.position.x);
                 SetOrient(angle * Mathf.Rad2Deg);
             }
-
+            //Same thing here, but we have a timer for calculate just one time
             if (GetSpaceShip().Velocity.magnitude < 0.2f && time_recalc_escapePoint_tmp >= characterControl.time_recalc_escapePoint)
-            {
                 New_Point_Calc(animator);
-            }
-
-        }
-
-        //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-
         }
 
         public void New_Point_Calc(Animator animator)
         {
             RaycastHit2D hit = Physics2D.Raycast(GetSpaceShip().transform.position, GetSpaceShip().transform.TransformDirection(Vector3.right), 3.5f, LayerMask.GetMask("Asteroid"));
-
             time_recalc_escapePoint_tmp = 0;
 
             float radius = 0;
-
             if (hit.collider != null)
                 radius = hit.collider.GetComponent<CircleCollider2D>().radius * 1.3f;
 
@@ -75,19 +56,13 @@ namespace FGAE
             float degre_b;
 
             if (animator.GetBool("berserk_mode"))
-            {
                 degre_b = Mathf.Atan2((characterControl.ss_target - GetSpaceShip().transform.position).y, (characterControl.ss_target - GetSpaceShip().transform.position).x) * Mathf.Rad2Deg;
-            }
             else
-            {
                 degre_b = Mathf.Atan2((characterControl.ss_target - GetSpaceShip().transform.position).y, (characterControl.ss_target - GetSpaceShip().transform.position).x) * Mathf.Rad2Deg;
-            }
             
 
             if (degre_b < 0)
-            {
                 degre_b = 360 + degre_b;
-            }
             RaycastHit2D avoid_r;
             RaycastHit2D avoid_l;
             float degre_r = 5;
@@ -96,28 +71,21 @@ namespace FGAE
             while (!out_b)
             {
                 avoid_r = Physics2D.Raycast(GetSpaceShip().transform.position, DegreeToVector2(degre_b + degre_r), hit.distance + radius, LayerMask.GetMask("Asteroid"));
-
                 if (avoid_r.collider == null)
-                {
                     out_b = true;
-                }
                 else
                 {
                     pos1 = avoid_r.point;
                     degre_r += 5;
                     Debug.DrawRay(GetSpaceShip().transform.position, (pos1 - GetSpaceShip().transform.position), Color.blue, 2);
                 }
-
             }
             out_b = false;
             while (!out_b)
             {
                 avoid_l = Physics2D.Raycast(GetSpaceShip().transform.position, DegreeToVector2(degre_b - degre_l), hit.distance + radius, LayerMask.GetMask("Asteroid"));
-
                 if (avoid_l.collider == null)
-                {
                     out_b = true;
-                }
                 else
                 {
                     pos2 = avoid_l.point;
@@ -130,13 +98,9 @@ namespace FGAE
 
             if (Vector3.Distance(GetSpaceShip().transform.position, pos1) + Vector3.Distance(pos1, (Vector3)GetCharacterControl(animator).spaceShip_FGAE.GetComponent<Rigidbody2D>().velocity + GetSpaceShip().transform.position) * 30
                 > Vector3.Distance(GetSpaceShip().transform.position, pos2) + Vector3.Distance(pos2, (Vector3)GetCharacterControl(animator).spaceShip_FGAE.GetComponent<Rigidbody2D>().velocity + GetSpaceShip().transform.position) * 30)
-            {
                 characterControl.avoinding_pos = pos2;
-            }
             else
-            {
                 characterControl.avoinding_pos = pos1;
-            }
             Debug.DrawRay(GetSpaceShip().transform.position, (characterControl.avoinding_pos - GetSpaceShip().transform.position), Color.red, 2);
         }
     }
--- Assets/Search_Points.cs
+++ Assets/Search_Points.cs
@@ -15,7 +15,6 @@ namespace FGAE
         public WayPoint targetCheck;
 
         public float angleTest;
-        public float test;
 
         private float delay_shot_tmp;
         public float threeshold_distancePoint;
@@ -25,10 +24,6 @@ namespace FGAE
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             GetCharacterControl(animator);
-            /*if (GetSpaceShip().name == "SpaceShip1")
-            {
-                animator.SetBool("berserk_mode", true);
-            }*/
             string ss_name = "";
             if (GetSpaceShip().name == "SpaceShip2")
             {
@@ -54,45 +49,31 @@ namespace FGAE
                 delay_shot_tmp -= Time.deltaTime;
             }
 
+            //Select a point to target, if the point is in a field of view, front of him, and he's not the owner of it
             Debug.DrawRay(GetSpaceShip().transform.position, new Vector3(GetSpaceShip().Velocity.x - GetSpaceShip().transform.right.x, GetSpaceShip().Velocity.y - GetSpaceShip().transform.right.y, 0), Color.yellow);
-
             if (!targetConfirmed)
             {
                 dist_detection = float.MaxValue;
-                //target = GameManager.Instance.GetGameData().WayPoints[count];
                 foreach (WayPoint point in GetData().WayPoints)
                 {
                     float distance = Vector3.Distance(GetSpaceShip().transform.position, point.transform.position);
                     angleTest = Vector3.Angle(point.transform.position - GetSpaceShip().transform.position, GetSpaceShip().transform.right);
-                    if (distance < dist_detection && distance < threeshold_distancePoint && (Vector3.Dot(GetSpaceShip().transform.right, point.transform.position - GetSpaceShip().transform.position) > 0) &&  point.Owner != GetSpaceShip().Owner && angleTest < 80 || angleTest <-80)
+                    if (distance < dist_detection && distance < threeshold_distancePoint && (Vector3.Dot(GetSpaceShip().transform.right, point.transform.position - GetSpaceShip().transform.position) > 0) &&  point.Owner != GetSpaceShip().Owner && angleTest < 65 || angleTest <-65)
                     {
                         targetCheck = point;
                         dist_detection = distance;
                     }
-                    //EASY AI//
-                    /*
-                    else if (distance < dist_detection && distance < threeshold_distancePoint && (Vector3.Dot(GetSpaceShip().transform.right, point.transform.position - GetSpaceShip().transform.position) > 0) && point.Owner != GetSpaceShip().Owner)
-                    {
-                        Debug.Log("no sabe que se");
-                        targetCheck = point;
-                        dist_detection = distance;
-                    }
-                    else if(distance < dist_detection && (Vector3.Dot(GetSpaceShip().transform.right, point.transform.position - GetSpaceShip().transform.position) > 0) && point.Owner != GetSpaceShip().Owner)
-                    {
-                        targetCheck = point;
-                        dist_detection = distance;
-                    }*/
-
                 }
                 characterControl.target = targetCheck;
                 targetConfirmed = true;
             }
 
+            //If his target is behind him, he choose an other point but he's not in the state of avoiding cause in this state he can turn around an asteroid before come back to the target
             if ((Vector3.Dot(GetSpaceShip().transform.right, characterControl.target.transform.position - GetSpaceShip().transform.position) < 0) && !animator.GetBool("avoiding"))
                 targetConfirmed = false;
 
+            //The spaceship has a raycastHit just for mines, if he detect one then he shoot on it 
             RaycastHit2D hit_mine = Physics2D.Raycast(GetSpaceShip().transform.position, GetSpaceShip().transform.TransformDirection(Vector3.right), 2, LayerMask.GetMask("Mine"));
-
             if (hit_mine.collider != null)
             {
                 if (delay_shot_tmp <= 0)
@@ -102,71 +83,59 @@ namespace FGAE
                 }
             }
 
+            //He use a raycastHit for asteroid, he detect asteroid on his way, then he try to avoid them
             RaycastHit2D hit = Physics2D.Raycast(GetSpaceShip().transform.position, GetSpaceShip().transform.TransformDirection(Vector3.right), 3.5f, LayerMask.GetMask("Asteroid"));
-
-            Vector3 ss_position = GetSpaceShip().transform.position;
-
             if (hit.collider != null)
             {
-                //avoiding = true;
                 Vector3 forward = GetSpaceShip().transform.right;
                 Vector3 toOther = hit.collider.transform.position - GetSpaceShip().transform.position;
                 Vector3 toOther2 = characterControl.target.transform.position - GetSpaceShip().transform.position;
+                //If the target and the obstacle are front of him then he avoid the obstacle else he ignore the obstacle
                 if (Vector3.Dot(forward, toOther) > 0 && Vector3.Dot(forward, toOther2) > 0)
                 {
                     animator.SetBool("avoiding", true);
                 }
-                else
-                {
-                    var velocityDirection = new Vector3(GetSpaceShip().transform.right.x - GetSpaceShip().Velocity.x, GetSpaceShip().transform.right.y - GetSpaceShip().Velocity.y, 0);
-                    velocityDirection = Vector3.Reflect(velocityDirection, GetSpaceShip().transform.right);
-
-                    if (Vector3.Distance(characterControl.target.transform.position, GetSpaceShip().transform.position) < 3f)
-                        test = Mathf.Atan2(characterControl.target.transform.position.y - ss_position.y, characterControl.target.transform.position.x - ss_position.x);
-                    else
-                        test = Mathf.Atan2(characterControl.target.transform.position.y - ss_position.y + velocityDirection.y, characterControl.target.transform.position.x - ss_position.x + velocityDirection.x);
-                    SetOrient(test * Mathf.Rad2Deg);
-                }
-
+                else//Inertia Calcul allow the spaceShip to targeta point against the inertia
+                    InertiaCalcul();
             }
             else
-            {
-                var velocityDirection = new Vector3(GetSpaceShip().transform.right.x - GetSpaceShip().Velocity.x, GetSpaceShip().transform.right.y - GetSpaceShip().Velocity.y, 0);
-                velocityDirection = Vector3.Reflect(velocityDirection, GetSpaceShip().transform.right);
+                InertiaCalcul();
 
-                if (Vector3.Distance(characterControl.target.transform.position, GetSpaceShip().transform.position) < 3f)
-                        test = Mathf.Atan2(characterControl.target.transform.position.y - ss_position.y, characterControl.target.transform.position.x - ss_position.x);
-                else
-                        test = Mathf.Atan2(characterControl.target.transform.position.y - ss_position.y + velocityDirection.y, characterControl.target.transform.position.x - ss_position.x + velocityDirection.x);
-                SetOrient(test * Mathf.Rad2Deg);
-            }
-
+            //If he's a owner of his target, then he choose an other point
             if (characterControl.target.Owner == GetSpaceShip().Owner)
                 targetConfirmed = false;
 
+            //If he enter in a point an he's a owner of him then he drop a mine on it
             if (Physics2D.OverlapCircle(characterControl.target.Position, characterControl.target.Radius, LayerMask.GetMask("Player")) && characterControl.target.Owner == GetSpaceShip().Owner)
                 SetMine(true);
 
-
+            //If the AI score is too different then he enter in berserk mode, he track the player whenever the enemy is
             float angle_ss2 = Vector3.Angle(ss2.transform.position - GetSpaceShip().transform.position, GetSpaceShip().transform.right);
-
             if (GameManager.Instance.GetScoreForPlayer(get_index_spaceship()) - GameManager.Instance.GetScoreForPlayer(get_enemy_index_spaceship()) > characterControl.dif_score)
-            {
                 animator.SetBool("berserk_mode", true);
-            }
             else
-            {
-                if (angle_ss2 < characterControl.view_field && Vector2.Distance(GetSpaceShip().transform.position, ss2.transform.position) < characterControl.distance_view_field)
-                {
+                if (angle_ss2 < characterControl.view_field && Vector2.Distance(GetSpaceShip().Position, ss2.transform.position) < characterControl.distance_view_field)
                     animator.SetBool("attack_mode", true);
-                }
-            }
 
-            float dist_ = Vector2.Distance(ss2.transform.position, GetSpaceShip().transform.position);
+            //If the enemy within a range and behind, he drop the bomb
+            float dist_ = Vector2.Distance(ss2.transform.position, GetSpaceShip().Position);
             if (angle_ss2 > characterControl.view_field_mine &&  dist_ >  characterControl.dist_min_mine && dist_ < characterControl.dist_max_mine && GetSpaceShip().Energy > characterControl.energy_min_mine)
-            {
                 SetMine(true);
-            }
+        }
+
+        void InertiaCalcul()
+        {
+            var velocityDirection = new Vector3(GetSpaceShip().transform.right.x - GetSpaceShip().Velocity.x, GetSpaceShip().transform.right.y - GetSpaceShip().Velocity.y, 0);
+            velocityDirection = Vector3.Reflect(velocityDirection, GetSpaceShip().transform.right);
+
+            float angle;
+            if (Vector3.Distance(characterControl.target.transform.position, GetSpaceShip().transform.position) < 3f)
+                angle = Mathf.Atan2(characterControl.target.transform.position.y - GetSpaceShip().transform.position.y, characterControl.target.transform.position.x - GetSpaceShip().transform.position.x);
+            else
+                angle = Mathf.Atan2(characterControl.target.transform.position.y - GetSpaceShip().transform.position.y + velocityDirection.y, characterControl.target.transform.position.x - GetSpaceShip().transform.position.x + velocityDirection.x);
+            SetOrient(angle * Mathf.Rad2Deg);
         }
     }
+
+    
 }
